251fcf8d5aba89b4a54fa1253388f653acf0537a
adrianfer20 <adrianfermartinez6@gmail.com>
2023-07-30 22:19:57 -0400

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./main.css"
+function App() {
+
+  return (
+    <>
+      <div className='min-h-screen bg-slate-900 text-slate-100'>
+        <h1 className='text-2xl'>Hola, mundo</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
